abf11a413f4369d68c4f7838616df507bdd25d75
4goUnited <4goUnited@gmail.com>
2022-02-18 19:33:31 +0500

    My first solution for homework-2

--- solution.go
+++ solution.go
@@ -8,5 +8,21 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+import "math"
+
+type mySidesNumber int
+
+const SidesTriangle mySidesNumber = 3
+const SidesSquare mySidesNumber = 3
+const SidesCircle mySidesNumber = 0
+
+func CalcSquare(sideLen float64, sidesNum mySidesNumber) float64 {
+	switch sidesNum {
+	case SidesTriangle:
+		return math.Sqrt(3) * sideLen * sideLen / 4
+	case SidesSquare:
+		return sideLen * sideLen
+	case SidesCircle:
+		return math.Pi * sideLen * sideLen
+	}
 }
